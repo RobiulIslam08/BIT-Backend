aeac460d1a6032e3b9231d4ceb72371aab707a21
Rabiul Islam <robiulislamrobi0874@gmail.com>
2026-06-23 12:50:57 +0600

    login and register

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,12 @@ const app: Application = express();
 //parser
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true,
+  }),
+);
 app.use('/api/v1', router);
 app.get('/', (req: Request, res: Response) => {
   res.json({
